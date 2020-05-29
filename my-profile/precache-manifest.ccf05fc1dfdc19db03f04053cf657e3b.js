0956d163428dea9016416f9459a218ff6b0cb3c8
Nguyen Phu Quy <quynguyen310193@gmail.com>
2020-05-29 14:08:29 +0700

    update my profile

---
+++ my-profile/precache-manifest.ccf05fc1dfdc19db03f04053cf657e3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222470da5b4b28b58bb02cbd784af8ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baa5f82d2774ec07e2ab",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a6f47b458043ced7c609",
+    "url": "/static/js/2.a8ffd96f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8ffd96f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa5f82d2774ec07e2ab",
+    "url": "/static/js/main.961d1b99.chunk.js"
+  },
+  {
+    "revision": "b38f709691f005f4c585",
+    "url": "/static/js/runtime-main.e2af1984.js"
+  }
+]);
